43b92d170779814380e1f43833d6ee23a635ea68
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-22 21:11:10 -0300

    Create acao.php

---
+++ php/acao.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<?php
+    $comando = "";
+    if(isset($_POST['comando'])){$comando = $_POST['comando'];}else if(isset($_GET['comando'])){$comando = $_GET['comando'];}
+    $tabela = "";
+    if(isset($_POST['tabela'])){$tabela = $_POST['tabela'];}else if(isset($_GET['tabela'])){$tabela = $_GET['tabela'];}
+?>
+<html>
+<body class="">
+<?php
+    include_once "../conf/default.inc.php";
+    require_once "../conf/Conexao.php";
+    acao($comando, $tabela);
+
+    function acao($acao, $tabela){
+        if($tabela == "cidade"){
+            require_once "Cidade.class.php";
+        } else if($tabela == "estado"){
+            require_once "Estado.class.php";
+        }
+        if($acao == "insert"){
+            if($tabela == "cidade"){
+                $cidade = new Cidade("", $_POST['CidadeNome'], $_POST['EstadoID']);
+                $resultado = $cidade->inserir();
+                if ($resultado){
+                    header("location:tabelacidade.php");
+                } else {echo "Não funcionou";}
+            } else if($tabela == "estado") {
+                $estado = new Estado("", $_POST['EstadoNome'], $_POST['EstadoSigla']);
+                $resultado = $estado->inserir();
+                if ($resultado){
+                    header("location:tabelaestado.php");
+                } else {echo "Não funcionou";}
+            }
+        }
+        else if($acao == "deletar"){
+            if($tabela == "cidade"){
+                $cidade = new Cidade($_GET['id'], "", "");
+                $resultado = $cidade->deletar();
+                if ($resultado){
+                    header("location:tabelacidade.php");
+                } else {echo "Não funcionou";}
+            } else if($tabela == "estado") {
+                $estado = new Estado($_GET['id'], "", "");
+                $resultado = $estado->deletar();
+                if ($resultado){
+                    header("location:tabelaestado.php");
+                } else {echo "Não funcionou";}
+            }
+        }
+        else if($acao == "update"){
+            if($tabela == "cidade"){
+                $cidade = new Cidade($_POST['id'], $_POST['CidadeNome'], $_POST['EstadoID']);
+                $resultado = $cidade->atualizar();
+                header("location:tabelacidade.php");
+            } else if($tabela == "estado") {
+                $estado = new Estado($_POST['id'], $_POST['EstadoNome'], $_POST['EstadoSigla']);
+                $resultado = $estado->atualizar();
+                header("location:tabelaestado.php");
+            }
+        }
+    }
+
+
+
+
+    
+
+    function dados(){
+        $dados = array();
+        $dados['CidadeID'] = $_POST["CidadeID"];
+        $dados['CidadeNome'] = $_POST["CidadeNome"];
+        $dados['EstadoID'] = $_POST["EstadoID"];
+        $dados['EstadoNome'] = $_POST["EstadoNome"];
+        $dados['EstadoSigla'] = $_POST["EstadoSigla"];
+        return $dados;
+    }
+
+
+    function buscarDados($id,$tabela){
+        $pdo = Conexao::getInstance();
+        $dados = array();
+    if($tabela == 'cidade'){
+        $consulta = $pdo->query("SELECT * FROM cidade, estado WHERE CidadeID = $id");
+        while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            $dados['CidadeNome'] = $linha['CidadeNome'];
+            $dados['EstadoID'] = $linha['EstadoID'];
+        }
+    } else if($tabela == 'estado'){
+        $consulta = $pdo->query("SELECT * FROM estado WHERE EstadoID = $id");
+        while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            $dados['EstadoNome'] = $linha['EstadoNome'];
+            $dados['EstadoSigla'] = $linha['EstadoSigla'];
+        }
+    }
+        return $dados;
+    }
+?>
+</body>
+</html>
